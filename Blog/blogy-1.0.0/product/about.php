7139ad5b94e7e58506255927306b80842fe4ed75
QUANG <quangnguyenkz123@gmail.com>
2023-12-05 08:35:54 +0700

    [PHP]-Trang about us
    xóa template new_admin

--- Blog/blogy-1.0.0/product/about.php
+++ Blog/blogy-1.0.0/product/about.php
@@ -3,7 +3,7 @@
 			<div class="row same-height justify-content-center">
 				<div class="col-md-6">
 					<div class="post-entry text-center">
-						<h1 class="mb-4">About Us</h1>
+						<h1 class="mb-4">Về chúng tôi</h1>
 					</div>
 				</div>
 			</div>
@@ -17,154 +17,136 @@
 
 				</div>
 				<div class="text">
-					<h2 class="heading text-primary mb-3">Resources for makers and creatives</h2>
-					<p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-					<p><a href="#" class="btn btn-outline-primary py-2">Read more</a></p>
+					<h2 class="heading text-primary mb-3">Tài nguyên dành cho người sáng tạo và người sáng tạo</h2>
+					<p class="mb-4">Cùng cộng động polyblog phát triễn cho những nhà sáng tạo trẻ</p>
 				</div>
-			</div>
 
-			<div class="half-content d-lg-flex align-items-stretch">
-				<div class="img order-md-2" style="background-image: url('images/hero_2.jpg')" data-aos="fade-in">
+				<div class="half-content d-lg-flex align-items-stretch">
+					<div class="img order-md-2" style="background-image: url('images/hero_2.jpg')" data-aos="fade-in">
 
-				</div>
-				<div class="text">
-					<h2 class="heading text-primary mb-3">We are trusted by more than 5,000 clients</h2>
-					<p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-					<p><a href="#" class="btn btn-outline-primary py-2">Read more</a></p>
-				</div>
-			</div>
-		</div>
-
-	</div>
-
-	<div class="section sec-features">
-		<div class="container">
-			<div class="row g-5">
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="0">
-					<div class="feature d-flex">
-						<span class="bi-bag-check-fill"></span>
-						<div>
-							<h3>Building your blog</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-						</div>
 					</div>
-				</div>
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="100">
-					<div class="feature d-flex">
-						<span class="bi-wallet-fill"></span>
-						<div>
-							<h3>Resources and insights</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-						</div>
+					<div class="text">
+						<h2 class="heading text-primary mb-3">Chúng tôi được hơn 1.000 người dùng tin tưởng</h2>
+						<p class="mb-4">Đạt mốc 10000 người dùng trong tháng</p>
 					</div>
-				</div>
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="200">
-					<div class="feature d-flex">
-						<span class="bi-pie-chart-fill"></span>
-						<div>
-							<h3>Blog just for you</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-
-	<div class="section">
-		<div class="container">
-
-			<div class="row mb-5">
-				<div class="col-lg-5 mx-auto text-center" data-aos="fade-up">
-					<h2 class="heading text-primary">Our Team</h2>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="0">
-					<img src="images/person_1.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
-					<h5 class="text-black">James Griffin</h5>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-				<div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="100">
-					<img src="images/person_2.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
-					<h5 class="text-black">Claire Smith</h5>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-				<div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="200">
-					<img src="images/person_3.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
-					<h5 class="text-black">Jessica Wilson</h5>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-
-				<div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="0">
-					<img src="images/person_4.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
-					<h5 class="text-black">William Anderson</h5>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-				<div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="100">
-					<img src="images/person_5.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
-					<h5 class="text-black">Julie Harvey</h5>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-				<div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="200">
-					<img src="images/person_2.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
-					<h5 class="text-black">Shana Clarkson</h5>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
 				</div>
 			</div>
 
 		</div>
-	</div>
+
+		<div class="section sec-features">
+			<div class="container">
+				<div class="row g-5">
+					<div class="col-12 col-sm-6 col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="0">
+						<div class="feature d-flex">
+							<span class="bi-bag-check-fill"></span>
+							<div>
+								<h3>Xây dựng blog của bạn</h3>
+								<p>Sáng tạo nội dung riêng theo cách của bạn</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-sm-6 col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+						<div class="feature d-flex">
+							<span class="bi-wallet-fill"></span>
+							<div>
+								<h3>Tài nguyên và hiểu biết sâu sắc</h3>
+								<p>Cung cấp tài nguyên từ những blog khác</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-sm-6 col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="200">
+						<div class="feature d-flex">
+							<span class="bi-pie-chart-fill"></span>
+							<div>
+								<h3>Blog chỉ dành cho bạn</h3>
+								<p>Nơi cho bạn thông tin đầy đủ mà bạn cần</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="section">
+			<div class="container">
+
+				<div class="row mb-5">
+					<div class="col-lg-5 mx-auto text-center" data-aos="fade-up">
+						<h2 class="heading text-primary">Our Team</h2>
+						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="0">
+						<img src="images/person_1.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
+						<h5 class="text-black">Nguyễn Đoàn Quang</h5>
+						<p></p>
+					</div>
+					<div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="100">
+						<img src="images/person_2.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
+						<h5 class="text-black">Tô Bằng Hiểu</h5>
+						<p></p>
+					</div>
+					<div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="200">
+						<img src="images/person_3.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
+						<h5 class="text-black">Hà Thị Phương Ngân</h5>
+						<p></p>
+					</div>
+				</div>
+
+			</div>
+		</div>
 
 
 
-	<div class="section">
-		<div class="container">
-			<div class="row justify-content-between">
-				<div class="col-lg-7 mb-4 mb-lg-0">
-					<img src="images/img_7_sq.jpg" alt="Image" class="img-fluid rounded
+		<div class="section">
+			<div class="container">
+				<div class="row justify-content-between">
+					<div class="col-lg-7 mb-4 mb-lg-0">
+						<img src="images/img_4_sq.jpg" alt="Image" class="img-fluid rounded
 					">
+						">
+					</div>
+					<div class="col-lg-4 ps-lg-2">
+						<div class="mb-5">
+							<h2 class="text-black h4">Nền tảng xuất bản dành cho các blogger chuyên nghiệp</h2>
+							<p>Cho các nhà sáng tạo nội dung về blog của mình</p>
+						</div>
+						<div class="d-flex mb-3 service-alt">
+							<div>
+								<span class="bi-wallet-fill me-4"></span>
+							</div>
+							<div>
+								<h3>Xây dựng blog của bạn</h3>
+								<p></p>
+							</div>
+						</div>
+
+						<div class="d-flex mb-3 service-alt">
+							<div>
+								<span class="bi-pie-chart-fill me-4"></span>
+							</div>
+							<div>
+								<h3>Tài nguyên và hiểu biết sâu sắc</h3>
+								<p></p>
+							</div>
+						</div>
+
+						<div class="d-flex mb-3 service-alt">
+							<div>
+								<span class="bi-bag-check-fill me-4"></span>
+							</div>
+							<div>
+								<h3>Blog chỉ dành cho bạn</h3>
+								<p></p>
+							</div>
+						</div>
+
+					</div>
+
 				</div>
-				<div class="col-lg-4 ps-lg-2">
-					<div class="mb-5">
-						<h2 class="text-black h4">Publishing platform for professional bloggers</h2>
-						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-					</div>
-					<div class="d-flex mb-3 service-alt">
-						<div>
-							<span class="bi-wallet-fill me-4"></span>
-						</div>
-						<div>
-							<h3>Building your blog</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-						</div>
-					</div>
-
-					<div class="d-flex mb-3 service-alt">
-						<div>
-							<span class="bi-pie-chart-fill me-4"></span>
-						</div>
-						<div>
-							<h3>Resources and insights</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-						</div>
-					</div>
-
-					<div class="d-flex mb-3 service-alt">
-						<div>
-							<span class="bi-bag-check-fill me-4"></span>
-						</div>
-						<div>
-							<h3>Blog just for you</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-						</div>
-					</div>
-
-				</div>
-
 			</div>
 		</div>
-	</div>
